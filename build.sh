3838efe5e7e314e431c8ecd237b72c2bf1a488e4
Michael Verhulst <michael@terminallabs.com>
2019-06-18 01:29:53 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 vagrant up
-vagrant ssh --command 'cd /vagrant; sudo bash bootstrap.sh'
+vagrant ssh --command 'cd /vagrant; sudo bash install_prereqs.sh'
+vagrant ssh --command 'cd /vagrant; sudo bash install_python.sh'
+vagrant ssh --command 'cd /vagrant; sudo bash install_pip_requirements.sh'
