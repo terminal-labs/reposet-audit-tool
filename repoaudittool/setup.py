98fba1de1da0fa8e3133fb914bf18579a04612e1
Michael Verhulst <michael@terminallabs.com>
2020-01-11 18:57:42 -0600

    Update setup.py

--- repoaudittool/setup.py
+++ repoaudittool/setup.py
@@ -18,7 +18,6 @@ setup(
     include_package_data=True,
     install_requires=[
         "utilities-package@git+https://gitlab.com/terminallabs/utilitiespackage/utilities-package.git@master#egg=utilitiespackage&subdirectory=utilitiespackage",
-        "PyGithub",
     ],
     entry_points="""
         [console_scripts]
